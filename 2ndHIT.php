8c9ef86308105a270c430deaa523d170ce6a9220
stefanthorpe <stefan.thorpe@gmail.com>
2014-05-05 18:48:46 +0800

    added the file that checks and polls queues

---
+++ 2ndHIT.php
@@ -0,0 +1,39 @@
+<?php
+    require_once(__DIR__.'/Turk50/Turk50.php');
+    include(__DIR__.'/aws-credentials.php');
+    require(__DIR__.'vendor/autoload.php');
+    
+    use Aws\Sqs\SqsClient;
+    
+    $client = SqsClient::factory(array(
+        "key" => $AWSAccessKeyId,
+        "secret" => $AWSSecretAccessKeyId,
+        "region" => "us-west-1"
+    ));
+    
+    $queueUrl = "Input the Queue end point";
+    
+    $result = $client->receiveMessage(array(
+        "QueueUrl" => $queueUrl
+    ));
+
+    foreach ($result->getPath('Messages/*/Body') as $messageBody) {
+        // Find HITId in body and check all assignments complete
+        echo $messageBody;
+    }
+        
+    function checkHITCompleted ($HITId) {
+
+        $turk50 = new Turk50($AWSAccessKeyId, $AWSSecretAccessKeyId);
+
+        //prepare Request
+        $Request = array(
+         "HITId" => $HITId
+         "AssignmentStatus" => "Approved"
+        );
+
+        // invoke CreateHIT
+        $RegResponse = $turk50->GetAssignmentsForHIT($Request);
+        echo ($RegResponse['HITTypeId'];
+   }
+?>
