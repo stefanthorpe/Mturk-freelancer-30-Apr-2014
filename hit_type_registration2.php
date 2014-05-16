0e33474a1630e1474eb8ef9ab253e27bc13c3c1e
stefanthorpe <stefan.thorpe@gmail.com>
2014-05-16 15:50:47 +0800

    added 2nd hit type reg

---
+++ hit_type_registration2.php
@@ -0,0 +1,31 @@
+<?php
+    require_once(__DIR__.'/Turk50/Turk50.php');
+    include(__DIR__.'/aws-credentials.php');
+
+// Creates a HitType and prints new ID
+
+        $turk50 = new Turk50($keys["AWSAccessKeyIdMturk"], $keys["AWSSecretAccessKeyIdMturk"], array("trace" => TRUE));
+
+        // require Worker_PercentAssignmentsApproved >= 90%
+        $Worker_PercentAssignmentsApproved = array(
+         "QualificationTypeId" => "000000000000000000L0",
+         "Comparator" => "GreaterThanOrEqualTo",
+         "IntegerValue" => "90"
+        );
+          
+        //prepare Request
+        $Request = array(
+         "Title" => "Forum Comments Hit 2",
+         "Description" => "Review a comment for a forum thread",
+         "Reward" => array("Amount" => "1.00", "CurrencyCode" => "USD"),
+         "AssignmentDurationInSeconds" => "60",
+         "LifetimeInSeconds" => "172800",
+         "AutoApprovalDelayInSeconds" => "43200",
+         "QualificationRequirement" => $Worker_PercentAssignmentsApproved
+        );
+
+        // invoke CreateHIT
+        $RegResponse = $turk50->RegisterHITType($Request);
+        $LastResponse = $turk50->__getLastResponse();
+        print($LastResponse["HITTypeId"]);
+?>
